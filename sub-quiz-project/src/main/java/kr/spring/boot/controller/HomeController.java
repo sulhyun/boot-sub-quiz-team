881d2e4aa357def687ac4a51251d7be770ffbf71
sulhyun <sulhyun4788@naver.com>
2024-11-16 14:42:25 +0900

    [hyun] 롬북 적용

---
+++ sub-quiz-project/src/main/java/kr/spring/boot/controller/HomeController.java
@@ -0,0 +1,19 @@
+package kr.spring.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home(Model model) {
+		model.addAttribute("name", "홍길동");
+		return "home";
+	}
+	
+}
--- sub-quiz-project/src/main/java/kr/spring/boot/main/BootApplication.java
+++ sub-quiz-project/src/main/java/kr/spring/boot/main/BootApplication.java
@@ -2,8 +2,10 @@ package kr.spring.boot.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "kr.spring.boot")
 public class BootApplication {
 
 	public static void main(String[] args) {
