0a54aeebe3e7622ec39e1223db2bfa691ff5398b
sulhyun <sulhyun4788@naver.com>
2024-11-16 16:49:45 +0900

    [hyun] DB 테스트, 레이아웃 적용

--- sub-quiz-project/src/main/java/kr/spring/boot/controller/HomeController.java
+++ sub-quiz-project/src/main/java/kr/spring/boot/controller/HomeController.java
@@ -1,19 +1,26 @@
 package kr.spring.boot.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import kr.spring.boot.dao.TestDAO;
+import kr.spring.boot.model.vo.QuizTypeVO;
 import lombok.AllArgsConstructor;
 
 @Controller
 @AllArgsConstructor
 public class HomeController {
 	
+	private TestDAO testDao;
 	
 	@GetMapping("/")
 	public String home(Model model) {
 		model.addAttribute("name", "홍길동");
+		List<QuizTypeVO> list = testDao.test();
+		System.out.println(list);
 		return "home";
 	}
 	
---
+++ sub-quiz-project/src/main/java/kr/spring/boot/dao/TestDAO.java
@@ -0,0 +1,11 @@
+package kr.spring.boot.dao;
+
+import java.util.List;
+
+import kr.spring.boot.model.vo.QuizTypeVO;
+
+public interface TestDAO {
+
+	List<QuizTypeVO> test();
+
+}
