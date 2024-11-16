dc21d5f91071bfbe846ae8f03d13c1ece8ecd225
sulhyun <sulhyun4788@naver.com>
2024-11-16 12:51:38 +0900

    [hyun] 프로젝트 시작

---
+++ sub-quiz-project/src/test/java/kr/spring/boot/SubQuizProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.spring.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubQuizProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
