cf9e8243dc75cecae2a9043e5467e7652cc4a54c
sulhyun <sulhyun4788@naver.com>
2024-11-18 15:08:16 +0900

    [hyun] 회원가입 기능 시작

---
+++ sub-quiz-project/src/main/java/kr/spring/boot/controller/MemberController.java
@@ -0,0 +1,22 @@
+package kr.spring.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.spring.boot.service.MemberService;
+import lombok.AllArgsConstructor;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+	private MemberService memberService;
+	
+	@GetMapping("/join")
+	public String join() {
+		
+		return "member/join";
+	}
+}
