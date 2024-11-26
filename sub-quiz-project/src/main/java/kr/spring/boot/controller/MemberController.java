9d0f60d12f5bcf2096feaf835f8f3fa4d37c1120
sulhyun <sulhyun4788@naver.com>
2024-11-27 00:19:28 +0900

    [hyun] 아이디 중복검사 기능 구현 완료

--- sub-quiz-project/src/main/java/kr/spring/boot/controller/MemberController.java
+++ sub-quiz-project/src/main/java/kr/spring/boot/controller/MemberController.java
@@ -1,9 +1,11 @@
 package kr.spring.boot.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.spring.boot.model.vo.MemberVO;
 import kr.spring.boot.service.MemberService;
 import lombok.AllArgsConstructor;
 
@@ -18,5 +20,12 @@ public class MemberController {
 	public String join() {
 		
 		return "member/signup";
+	}
+	
+	@GetMapping("/checkId")
+	public ResponseEntity<Boolean> checkId(String mb_id) {
+		MemberVO user = memberService.selectMember(mb_id);
+		boolean res = (user == null) ? true : false; 
+		return ResponseEntity.ok(res);
 	}
 }
---
+++ sub-quiz-project/src/main/java/kr/spring/boot/model/dto/SignupDTO.java
@@ -0,0 +1,24 @@
+package kr.spring.boot.model.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class SignupDTO {
+
+	private String mb_id;			// 회원 아이디
+	private String mb_pw;			// 회원 비밀번호
+	private String mb_name;			// 회원 이름
+	private String mb_nick;			// 회원 닉네임
+	private String mb_hp;			// 회원 전화번호
+	private String mb_email;		// 회원 이메일
+	private int mb_zip;				// 회원 우편번호
+	private String mb_addr;			// 회원 기본주소
+	private String mb_addr2;		// 회원 상세주소
+	private int mb_birth;			// 회원 생년월일
+	
+	// 초기값 설정
+	private int mb_level = 1;		// 회원 레벨
+	private int mb_point = 500;		// 회원 포인트
+}
