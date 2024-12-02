825cef47b444b0e5bfcde51fbb658125aa68a9cd
sulhyun <sulhyun4788@naver.com>
2024-12-03 01:29:37 +0900

    [hyun] 로그인 기능 구현 완료

--- sub-quiz-project/src/main/java/kr/spring/boot/handler/LoginFailHandler.java
+++ sub-quiz-project/src/main/java/kr/spring/boot/handler/LoginFailHandler.java
@@ -5,7 +5,6 @@ import java.net.URLEncoder;
 
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 import jakarta.servlet.ServletException;
@@ -16,20 +15,11 @@ public class LoginFailHandler implements AuthenticationFailureHandler  {
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-		System.out.println("로그인 실패 핸들러 작동중");
-		String errMsg = "";
-		
-		if(exception instanceof UsernameNotFoundException) {
-			System.out.println("UsernameNotFoundException 동작");
-			errMsg = exception.getMessage();
-			String encodingErrMsg = URLEncoder.encode(errMsg, "UTF-8");
-			response.sendRedirect("/member/login?error=true&msg=" + encodingErrMsg);
-		}
+		String errorMessage = "";
+
 		if(exception instanceof BadCredentialsException) {
-			System.out.println("BadCredentialsException 동작");
-			errMsg = "아이디 또는 비밀번호가 일치하지 않습니다.";
-			String encodingErrMsg = URLEncoder.encode(errMsg, "UTF-8");
-			response.sendRedirect("/member/login?error=true&msg=" + encodingErrMsg);
+			errorMessage = "아이디 또는 비밀번호가 일치하지 않습니다.";
+			response.sendRedirect("/member/login?error=true&msg=" + URLEncoder.encode(errorMessage, "UTF-8"));
 		}
 	}
 
