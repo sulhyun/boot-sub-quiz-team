cceb24424c6c1f83ae3275cb06c893756365632c
oiihyg <oiihyg217@gmail.com>
2024-12-05 13:50:55 +0900

    이벤트 페이지 수정중

--- sub-quiz-project/src/main/java/kr/spring/boot/dao/EventDAO.java
+++ sub-quiz-project/src/main/java/kr/spring/boot/dao/EventDAO.java
@@ -1,5 +1,7 @@
 package kr.spring.boot.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import kr.spring.boot.model.vo.EventVO;
@@ -7,6 +9,9 @@ import kr.spring.boot.model.vo.EventVO;
 @Mapper
 public interface EventDAO {
 
+
 	void evtaddpage(EventVO event);
+
+	List<EventVO> selectEvent();
 
 }
