7bfdbdaa4faae7c57f93aa82b65128cbab65d38f
oiihyg <oiihyg217@gmail.com>
2024-12-11 15:23:56 +0900

    Reapply "Merge branch 'main' into hyunho"
    This reverts commit ea2f89c89791335ec6e885826ef22057ff7ec41c.

--- sub-quiz-project/src/main/java/kr/spring/boot/controller/CommunityController.java
+++ sub-quiz-project/src/main/java/kr/spring/boot/controller/CommunityController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.spring.boot.model.vo.CommunityVO;
-import kr.spring.boot.service.CoummuService;
+import kr.spring.boot.model.vo.PostVO;
+import kr.spring.boot.service.CommuService;
 import lombok.AllArgsConstructor;
 
 @Controller
@@ -18,13 +20,29 @@ import lombok.AllArgsConstructor;
 public class CommunityController {
 
 	@Autowired
-	private CoummuService coummuService;
+	private CommuService commuService;
 	
 	// 커뮤니티 리스트업 
+	@GetMapping("/commuPage/{co_num}")
+	public String commuList(Model model, @PathVariable int co_num) {
+		List<PostVO> commu = commuService.getPostList(co_num);
+		model.addAttribute("commu", commu);
+		return "/community/commuPage";		
+	}
+	
+	// 커뮤니티 게시글 정보 리스트업 
+	@GetMapping("/commuDetail")
+	public String detailList(Model model) {
+
+		return "/community/commuDetail";		
+	}
+	
+	// 커뮤홈 - 카테고리 리스트업 
 	@GetMapping("/commuHome")
-	public String commuList(Model model) {
-		List<CommunityVO> coummu = coummuService.getAllcoummu();
-		model.addAttribute("coummu", coummu);
+	public String commuLisst(Model model) {
+		List<CommunityVO> commu = commuService.getAllcoummu();
+		model.addAttribute("commu", commu);
 		return "/community/commuHome";		
 	}
+	
 }
