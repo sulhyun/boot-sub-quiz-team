3ccdc16afd79482aa84a2d7d5f8af856dbebaf6c
sulhyun <sulhyun4788@naver.com>
2024-12-17 18:57:15 +0900

    [hyun] 데이터 베이스 수정 및 vo 클래스 수정

--- sub-quiz-project/src/main/java/kr/spring/boot/model/vo/QuizTypeVO.java
+++ sub-quiz-project/src/main/java/kr/spring/boot/model/vo/QuizTypeVO.java
@@ -8,6 +8,8 @@ import lombok.NoArgsConstructor;
 public class QuizTypeVO {
 	
 	private int qt_num;			// 퀴즈 유형 기본키
-	private String qt_name;		// 퀴즈 유형 명
+	private String qt_name;		// 퀴즈 카테고리 명
+	private String qt_type;		// 퀴즈 유형 명
+	private int ra_num;			// 점수 번호
 	
 }
